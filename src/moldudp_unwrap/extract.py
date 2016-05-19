5df95edf33681a67e9c68fb7da99f0e611e351dc
Douglas R Colkitt <douglas.colkitt@gmail.com>
2016-05-19 09:58:48 +0000

    Add support for end-of-stream

--- src/moldudp_unwrap/extract.py
+++ src/moldudp_unwrap/extract.py
@@ -1,17 +1,30 @@
 
 import struct
 
+def peek_ahead_packets_left (byte_source):
+    n_peek_bytes = 2
+    read_attempt = byte_source.read(n_peek_bytes)
+    return len(read_attempt) == n_peek_bytes
+
 def extract_msgs_from_byte_source (byte_source):
-    session_num = _consume_session_num(byte_source)
+    session_num = _consume_session_num_post_peek(byte_source)
     seq_num = _consume_seq_num(byte_source)
     msgs = _consume_packet_msgs(byte_source)
     return (session_num, seq_num, len(msgs))
 
-def _consume_session_num (byte_source):
-    n_session_bytes = 10
-    session_bytes = byte_source.read(n_session_bytes)
+def _consume_session_num_post_peek (byte_source):
+    session_bytes = byte_source.read(_get_session_read_bytes())
     session_parsable_bytes = _truncate_session_bytes_to_intable(session_bytes)
     return struct.unpack(">Q", session_parsable_bytes)[0]
+
+# We can only use up to 8 bytes for sesssion num, so it's alright to discard
+# 2 to peek-ahead at any packets left.
+def _get_session_read_bytes():
+    n_session_bytes = 10
+    return n_session_bytes - _get_packet_peek_bytes()
+
+def _get_packet_peek_bytes():
+    return 2    
 
 def _truncate_session_bytes_to_intable (session_num_bytes):
     return session_num_bytes[-8:]
@@ -32,6 +45,12 @@ def _scan_message (byte_source):
 
 def _pop_message_size (byte_source):
     return _read_field_size(byte_source)
+
+def _determine_message_count (byte_source):
+    count_field_val = _pop_message_count(byte_source)
+    end_of_stream_count = 65535
+    return count_field_val if \
+        count_field_val != end_of_stream_count else 0
 
 def _pop_message_count (byte_source):
     return _read_field_size(byte_source)
--- src/moldudp_unwrap/stream_parse.py
+++ src/moldudp_unwrap/stream_parse.py
@@ -5,6 +5,6 @@ import extract as ex
 
 in_stream = sys.stdin
 
-while True:
+while ex.peek_ahead_packets_left(in_stream):
     print ex.extract_msgs_from_byte_source(in_stream)
 
