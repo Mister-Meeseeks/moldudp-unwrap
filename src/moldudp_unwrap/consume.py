4bdcb72571928e73d20b9f27e71bbf30db7aeca4
Douglas R Colkitt <douglas.colkitt@gmail.com>
2016-05-20 05:08:31 +0000

    Added doc-strings

--- src/moldudp_unwrap/consume.py
+++ src/moldudp_unwrap/consume.py
@@ -1,8 +1,19 @@
 
+"""
+Library contains functions transforming MoldUDP packet streams variosu
+parsed inputs.
+"""
+
+
 import struct
 import extract as ex
 
 def packet_stream_to_message_stream (packet_stream, message_stream):
+    """ Reads from a package stream, then writes unpakced binary messages
+    to an output stream
+    Args: packet_stream - input stream or object supporting .read(int)
+          message_stream - output stream or object supporting .write(string)
+    """
     for msg in _iterate_message_from_packets(packet_stream):
         _write_message_stream(msg, message_stream)
 
@@ -12,6 +23,11 @@ def _write_message_stream(msg_str, msg_stream):
     msg_stream.write(msg_str)
 
 def format_packet_stream_headers (packet_stream):
+    """ Reads from a package stream, returns a generator of strings
+    describing packet header content.
+    Args: packet_stream - input stream or object supporting .read(int)
+    Returns: generator of formatted strings based on packet header
+    """
     for unpacked_packet in _iterate_packet_stream(packet_stream):
         yield _format_unpacked_headers(unpacked_packet)
 
