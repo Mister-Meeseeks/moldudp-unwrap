243c347c31ed9e9a931c807d359757f520ab376f
Douglas R Colkitt <colkitt@research-dev01.sith.com>
2016-10-13 20:06:25 -0400

    Add throttle to multicaster that spaces packets 1 ms apart

--- src/moldudp_unwrap/multicast.py
+++ src/moldudp_unwrap/multicast.py
@@ -1,4 +1,5 @@
 
+import time
 import socket
 
 def _open_multicast_socket():
@@ -10,6 +11,8 @@ class Multicaster:
     def __init__ (self, ip_addr, port):
         self._sock = _open_multicast_socket()
         self._dest = (ip_addr, port)
+        self._throttle_ms = 1
 
     def write (self, packet_bytes):
         self._sock.sendto(packet_bytes, self._dest)
+        time.sleep(self._throttle_ms / 1000.0)
