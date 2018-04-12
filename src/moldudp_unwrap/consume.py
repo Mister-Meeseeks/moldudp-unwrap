752d4fc788831bf0ca80a28cfcd030f5a23fa3ad
Douglas R Colkitt <colkitt@research-dev01.sith.com>
2018-04-11 21:14:24 -0400

    Add ability to subset output'd packets to range of sequence numbers

--- src/moldudp_unwrap/consume.py
+++ src/moldudp_unwrap/consume.py
@@ -8,13 +8,16 @@ parsed inputs.
 import struct
 import extract as ex
 
-def packet_stream_to_message_stream (packet_stream, message_stream):
+def packet_stream_to_message_stream (packet_stream, message_stream,
+                                     min_seq=None, max_seq=None):
     """ Reads from a package stream, then writes unpakced binary messages
     to an output stream
     Args: packet_stream - input stream or object supporting .read(int)
           message_stream - output stream or object supporting .write(string)
+          min_seq - Minimum seq number to cutoff output at (use None to ignore)
+          max_seq - Maximum seq number to cutoff output at (use None to ignore)
     """
-    for msg in _iterate_message_from_packets(packet_stream):
+    for msg in _iterate_messages_from_packet(packet_stream, min_seq, max_seq):
         _write_message_stream(msg, message_stream)
 
 def _write_message_stream(msg_str, msg_stream):
@@ -40,29 +43,35 @@ def format_packet_stream_headers (packet_stream):
     Args: packet_stream - input stream or object supporting .read(int)
     Returns: generator of formatted strings based on packet header
     """
-    for unpacked_packet in _iterate_packet_stream(packet_stream):
-        yield _format_unpacked_headers(unpacked_packet)
+    for unpacked in _iterate_packet_stream(packet_stream):
+        yield _format_unpacked_headers(unpacked)
 
-def format_header_msg_joins (packet_stream):
+def format_header_msg_joins (packet_stream, min_seq=None, max_seq=None):
     """ Reads from a package stream, returns a generator of strings
     describing the header content for every message (i.e. some headers
     be duplicated). Useful to line up headers with individual msgs. 
     Args: packet_stream - input stream or object supporting .read(int)
+          min_seq - Minimum seq number to cutoff output at (use None to ignore)
+          max_seq - Maximum seq number to cutoff output at (use None to ignore)
     Returns: generator of formatted strings based on packet header
     """
-    for unpacked_packet in _iterate_messages_as_packets(packet_stream):
-        yield _format_unpacked_headers(unpacked_packet)
+    for unpacked in _iterate_messages_as_packets\
+        (packet_stream, min_seq, max_seq):
+        yield _format_unpacked_headers(unpacked)
 
 
-def format_gap_bookend_headers (packet_stream):
+def format_gap_bookend_headers (packet_stream, min_seq=None, max_seq=None):
     """ Reads from a package stream, returns a generator of strings
     for messages that bookend gaps in the sequence number.
     Args: packet_stream - input stream or object supporting .read(int)
+          min_seq - Minimum seq number to cutoff output at (use None to ignore)
+          max_seq - Maximum seq number to cutoff output at (use None to ignore)
     Returns: generator of formatted strings based on packet header
     """
     last_seq = 0
     last_fmt = None
-    for (ses, seq, msgs) in _iterate_messages_as_packets(packet_stream):
+    for (ses, seq, msgs) in _iterate_messages_as_packets\
+        (packet_stream, min_seq, max_seq):
         fmt = _format_unpacked_headers((ses, seq, msgs))
         if (seq > last_seq + 1):
             yield _format_bookend(last_fmt, fmt)
@@ -79,18 +88,22 @@ def _format_bookend (open_fmt, close_fmt):
     close_paste = "> %s" % close_fmt
     return "\n".join([prefix, open_paste, close_paste, ""])
 
-def _iterate_message_from_packets (packet_stream):
-    for (_, _, msgs) in _iterate_packet_stream(packet_stream):
-        for msg in msgs:
-            yield msg    
-            
 def _iterate_packet_stream (packet_stream):
     while ex.peek_ahead_packets_left(packet_stream):
         yield ex.extract_msgs_from_byte_source(packet_stream)
-        
-def _iterate_messages_as_packets (packet_stream):
+
+def _iterate_messages_from_packets (packets, min_seq, max_seq):
+    for (_, _, msg) in _iterate_messages_as_packets(packets, min_seq, max_seq):
+        yield msg
+
+def _iterate_messages_as_packets (packet_stream, min_seq, max_seq):
     for (sess, seq, msgs) in _iterate_packet_stream(packet_stream):
         for msg in msgs:
-            yield (sess, seq, [msg])
+            if (in_seq_range(seq, min_seq, max_seq)):
+                yield (sess, seq, [msg])
             seq = seq + 1
             
+def in_seq_range (seq, min_seq, max_seq):
+    above_min = seq >= min_seq if min_seq is not None else True
+    below_max = seq <= max_seq if max_seq is not None else True
+    return above_min and below_max
--- src/moldudp_unwrap/moldudp.py
+++ src/moldudp_unwrap/moldudp.py
@@ -7,35 +7,44 @@ import argparse
 
 parser = argparse.ArgumentParser\
          (description="Unwrap MoldUDP Binary Packet Stream")
+parser.add_argument('--beginSeq', default="", help="Only echo messages with " +
+                    "seqNum >= to [arg]. Only works with normal mode")
+parser.add_argument('--endSeq', default="", help="Only echo messages with " +
+                    "seqNum <= than [arg]. Only works with normal mode")
 parser.add_argument('--header', action="store_true",
-                    help="Parse packet header fields, instead of body")
+                    help="Mode to parse packet header fields, instead of body")
 parser.add_argument('--msgHeader', action="store_true",
-                    help="Print header metadata associated with each message")
+                    help="Mode to print header metadata associated with " \
+                    + "each message")
 parser.add_argument('--gapSeq', action="store_true",
-                    help="Mode which only prints the headers of messages that"
+                    help="Print the header metadata of messages that"
                     + " bookend gaps in the sequence number.")
 parser.add_argument('--multicast', action="append",
                     help="Multicast parsed packets. Format: [IPAddr]:[Port]")
 parser.add_argument('--throttle', default="1",
                     help="Milliseconds between multicast transmits")
 parser.add_argument('--echo', action="store_true",
-                    help="Parse each echo then write bytes, acts as "
+                    help="Mode to parse and echo bytes back. Acts as "
                     + "'cat' unless parse broke. Used for testing")
 args = parser.parse_args()
 
 in_stream = sys.stdin
 out_stream = sys.stdout
 
+min_seq = int(args.beginSeq) if len(args.beginSeq) else None
+max_seq = int(args.endSeq) if len(args.endSeq) else None
+
 def read_and_print_headers (in_stream):
     for fmt_str in consume.format_packet_stream_headers(in_stream):
         print fmt_str
 
 def read_and_print_gap_headers (in_stream):
-    for fmt_str in consume.format_gap_bookend_headers(in_stream):
+    for fmt_str in consume.format_gap_bookend_headers\
+        (in_stream, min_seq, max_seq):
         print fmt_str
 
 def join_and_print_msg_headers (in_stream):
-    for fmt_str in consume.format_header_msg_joins(in_stream):
+    for fmt_str in consume.format_header_msg_joins(in_stream, min_seq, max_seq):
         print fmt_str
 
 def parse_multicast (dest_arg):
@@ -55,5 +64,6 @@ elif (args.multicast is not None and len(args.multicast) > 0):
     cast_socket = multicast.Multicaster(mult_dests, int(args.throttle))
     consume.split_packets_from_stream(in_stream, cast_socket)
 else:
-    consume.packet_stream_to_message_stream(in_stream, out_stream)
+    consume.packet_stream_to_message_stream\
+        (in_stream, out_stream, min_seq, max_seq)
 
